06bfdc23a69867b17dc14ec811653935ba601c34
Jaime Cárdenas <jaime.cardenas176@gmail.com>
2024-03-05 19:50:18 +0100

    gracias X nnada

--- productlist/productlist.js
+++ productlist/productlist.js
@@ -7,7 +7,7 @@ async function getAllProducts() {
         .then((responseJson) => {
             {
                 console.log(responseJson);
-                productsList = responseJson.products.data;
+                productsList = responseJson.data;
             }
 
         })
@@ -17,30 +17,54 @@ async function getAllProducts() {
 function createProductNode(product) {
 
     let item = document.createElement('li');
-    let contenedorDiv = document.createElement('div');
-    let category = document.createElement('p');
-    let price = document.createElement('h4');
-    let image = document.createElement('img');
+    item.className = 'item';
 
-    category.innerHTML = product.category;
+    let contenedorDiv = document.createElement('div');
+    contenedorDiv.className = 'card';
+
+    let name = document.createElement('p');
+    name.className = 'productName';
+
+    let  price= document.createElement('h4');
+    price.className = 'price';
+
+    let image = document.createElement('img');
+    image.className = 'productIMG';
+
+    name.innerHTML = product.name;
     price.innerHTML = product.price;
-    image.innerHTML = product.image;
-    contenedorDiv.appendChild(category);
-    contenedorDiv.appendChild(price);
+
+    //paranoyas del api preguntame
+    console.log('imagen pro',product.image );
+    product.image = product.image.replace('\\', '')
+    console.log('imagen pro AA',product.image );
+    image.src = product.image;
+
     contenedorDiv.appendChild(image);
+    contenedorDiv.appendChild(name);
+    contenedorDiv.appendChild(price);
+
+
     item.appendChild(contenedorDiv);
 
     return item;
 }
+
+
 listaProductos = document.getElementById('productlist');
 
-window.addEventListener('load', async () => {
-    await getAllProducts()
+
+function createAllProductsNodes(){
     for (let i = 0; i < productsList.length; i++) {
         const productHTML = createProductNode(productsList[i]);
         console.log(productHTML);
         listaProductos.appendChild(productHTML);
     }
+}
+
+window.addEventListener('load', async () => {
+    await getAllProducts()
+    createAllProductsNodes()
 });
 
 
